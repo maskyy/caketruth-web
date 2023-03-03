0a97a753d63f4fa12c37e9888a5216249c6503b0
maskyy <62175286+maskyy@users.noreply.github.com>
2023-03-03 09:24:35 +0300

    Add some routes, TODO footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import DiaryPage from "./pages/DiaryPage";
+import ProductsPage from "./pages/ProductsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 function App() {
-  return <div>hello world</div>;
+  return <div>
+    <h1>header 1</h1>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/diary">diary</Link></li>
+          <li><Link to="/products">products</Link></li>
+          <li><Link to="/profile">profile</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>hello world</div>} />
+        <Route path="/diary" element={<DiaryPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
---
+++ src/pages/DiaryPage.tsx
@@ -0,0 +1,5 @@
+function DiaryPage() {
+  return <div>diary</div>;
+}
+
+export default DiaryPage;
